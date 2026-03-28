6a5cdf11442af1c05b737d897094ac099c811e8c
uatob <support@uatob.com>
2026-03-28 14:29:43 -0400

    89

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  // reactCompiler: true, // ❌ remove this
 };
 
 export default nextConfig;
