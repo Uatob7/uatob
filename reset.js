ef2e764667402c71a33eb7a3a8c4d29625b14b1c
uatob <support@uatob.com>
2026-05-30 10:23:43 -0400

    ff

--- reset.js
+++ reset.js
@@ -1,9 +1,3 @@
 rm -rf node_modules
 rm package-lock.json
 
-for fn in $(gcloud functions list --region=us-east1 --format="value(name)")
-do
-  gcloud functions deploy $fn \
-    --region=us-east1 \
-    --memory=1Gi
-done
