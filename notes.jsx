20e306ef023f7956e7961b25e9c81e827b803ad7
uatob <support@uatob.com>
2026-06-06 06:47:20 -0400

    ddd

---
+++ notes.jsx
@@ -0,0 +1,192 @@
+
+API/Service Details
+To call this API from your own applications, you may need to create credentials.
+
+Cloud Run Admin API
+Deploy and manage user provided container images that scale automatically based on incoming requests. The Cloud Run Admin API v1 follows the Knative Serving API specification, while v2 is aligned with Google Cloud AIP-based API standards, as described in https://google.aip.dev/.
+
+By Google Enterprise API
+Service name
+run.googleapis.com
+Type
+Public API
+Status
+Enabled
+Documentation
+Explore
+Metrics
+Quotas & System Limits
+Credentials
+Cost
+
+Set up quota & system limit alerts
+Get alerted if a quota is close to reaching its maximum. Click on more actions button  in a row to get started, or click "Learn more" to view documentation.
+Current usage > 90%
+3
+View quotas & system limits
+7 day peak usage > 90%
+3
+View quotas & system limits
+All quotas & system limits
+838
+Filter
+Name
+Type
+Dimensions (e.g. location)
+Value
+Current usage percentage
+Current usage
+Adjustable 
+Total CPU allocation, in milli vCPU, per project per region	Quota	region : us-central1	20,000		
+100%
+20,000	Yes		
+Total CPU allocation, in milli vCPU, per project per region	Quota	region : us-east1	20,000		
+100%
+20,000	Yes		
+Number of regions that Cloud Run has been used in	Quota		3		
+100%
+3	Yes		
+Total memory allocation, in bytes, per project per region	Quota	region : us-central1	42,949,672,960 B (42.95 GB)		
+12.5%
+5,368,709,120 B (5.369 GB)	Yes		
+Total memory allocation, in bytes, per project per region	Quota	region : us-east1	42,949,672,960 B (42.95 GB)		
+12.5%
+5,368,709,120 B (5.369 GB)	Yes		
+Services per region	Quota	region : us-east1	1,000		
+5.5%
+55	Yes		
+Write requests per minute per region	Quota	region : us-east1	30		
+3.33%
+1	Yes		
+Write requests per minute per region	Quota	region : us-central1	30		
+3.33%
+1	Yes		
+Services per region	Quota	region : us-central1	1,000		
+2.7%
+27	Yes		
+Active Revisions per region	Quota	region : us-east1	4,000		
+1.35%
+54	Yes		
+Active Revisions per region	Quota	region : us-central1	4,000		
+0.68%
+27	Yes		
+Read requests per minute per region	Quota	region : us-east1	600		
+0.63%
+3.75	Yes		
+Read requests per minute per region	Quota	region : us-central1	600		
+0.5%
+3	Yes		
+Read requests per minute per region	Quota	region : africa-south1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-east1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-east2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-northeast1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-northeast2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-northeast3	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-south1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-south2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-southeast1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-southeast2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : asia-southeast3	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : australia-southeast1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : australia-southeast2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-central2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-north1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-north2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-southwest1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west10	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west12	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west3	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west4	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west6	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west8	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : europe-west9	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : me-central1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : me-west1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : northamerica-northeast1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : northamerica-northeast2	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : northamerica-south1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : southamerica-east1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : southamerica-west1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : us-east4	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : us-east5	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : us-south1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : us-west1	600		
+0.17%
+1	Yes		
+Read requests per minute per region	Quota	region : us-west2	600		
+0.17%
+1	Yes		
+Rows per page:
+50
+1 – 50 of 838
