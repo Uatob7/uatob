67372b578af984fd94f32195bede9c93aaf153a4
uatob <support@uatob.com>
2026-06-10 19:09:13 -0400

    ssss

--- src/App/UaTob/index.jsx
+++ src/App/UaTob/index.jsx
@@ -1,23 +1,18 @@
-import { useMemo } from "react";
-
-import { useRides }      from '@/App/UaTob/useRides';
-import { useSearch }     from "@/App/Drivers/useSearch";
-import { useAccounts }   from '@/App/UaTob/useAccounts';
-import { useCreateTrip } from '@/App/UaTob/useCreateTrip';
-import { useDrivers }    from '@/App/UaTob/useDrivers';
-import UaTob             from '@/App/UaTob/UaTobApp';
+import { useRides }           from '@/App/UaTob/useRides';
+import { useSearch }          from "@/App/Drivers/useSearch";
+import { useAccounts }        from '@/App/UaTob/useAccounts';
+import { useCreateTrip }      from '@/App/UaTob/useCreateTrip';
+import { useDrivers }         from '@/App/UaTob/useDrivers';
+import { useScheduledRides }  from '@/App/UaTob/useScheduledRides';
+import UaTob                  from '@/App/UaTob/UaTobApp';
 
 export default function App({ uid }) {
-  const { account }   = useAccounts(uid);
-  const { rides }     = useRides(uid);
-  const { createTrip } = useCreateTrip(uid);
-  const { searches }  = useSearch();
-  const { drivers }   = useDrivers();
-
-  const scheduledRides = useMemo(
-    () => rides.filter(r => r.isScheduled && r.status === 'scheduled'),
-    [rides]
-  );
+  const { account }        = useAccounts(uid);
+  const { rides }          = useRides(uid);
+  const { createTrip }     = useCreateTrip(uid);
+  const { searches }       = useSearch();
+  const { drivers }        = useDrivers();
+  const { scheduledRides } = useScheduledRides();
 
   return (
     <UaTob
